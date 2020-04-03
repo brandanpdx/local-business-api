0e8fe36e242041660dd60fd0a2faaafa5384a33e
Brandan Sayarath <brandan@brandan.tech>
2020-04-03 12:29:10 -0700

    add nswag package and middleware

--- LocalBusinessAPI/Startup.cs
+++ LocalBusinessAPI/Startup.cs
@@ -24,6 +24,9 @@ namespace LocalBusinessAPI
       services.AddDbContext<LocalBusinessAPIContext>(opt =>
           opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+      // Register the Swagger services
+      services.AddSwaggerDocument();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +41,10 @@ namespace LocalBusinessAPI
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
+
+      // Register the Swagger generator and the Swagger UI middlewares
+      app.UseOpenApi();
+      app.UseSwaggerUi3();
 
       // app.UseHttpsRedirection();
       app.UseMvc();
