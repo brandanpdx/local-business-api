10019f90eb9e747cc8b3407bb616a7aeca07619a
Brandan Sayarath <brandan@brandan.tech>
2020-04-03 11:20:30 -0700

    add initial seed data

--- LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
+++ LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
@@ -10,5 +10,13 @@ namespace LocalBusinessAPI.Models
     }
 
     public DbSet<Business> Businesses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+          .HasData(
+            new Business { BusinessId = 1, BusinessName = "United States", BusinessType = "New York City", BusinessAddress = "123 Main", OpeningHour = "11 AM", ClosingHour = "6 PM"}
+          );
+    }
   }
 }
