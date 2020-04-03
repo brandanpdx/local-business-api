ba9d3ee9de82c129c6c91a660b880bef1decc5a7
Brandan Sayarath <brandan@brandan.tech>
2020-04-03 11:38:34 -0700

    add migrations SeedData

---
+++ LocalBusinessAPI/Migrations/20200403183703_SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusinessAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Businesses",
+                columns: new[] { "BusinessId", "BusinessAddress", "BusinessName", "BusinessType", "ClosingHour", "OpeningHour" },
+                values: new object[,]
+                {
+                    { 1, "120 NE Russell St, Portland, OR 97212", "Toro Bravo", "Restaurant", "6 PM", "11 AM" },
+                    { 2, "960 SE 11th Ave, Portland, OR 97214", "Kachka", "Restaurant", "8 PM", "12 PM" },
+                    { 3, "225 SW Ash St, Portland, OR 97204", "BAES", "Restaurant", "11 PM", "11 AM" },
+                    { 4, "3012 NE Alberta St, Portland, OR 97211", "ROAR Cat Specialty Gift Shop", "Shop", "6 PM", "11 AM" },
+                    { 5, "40 NW 10th Ave, Portland, OR 97209", "MadeHere PDX", "Shop", "6 PM", "11 AM" },
+                    { 6, "2340 NE Sandy Blvd, Portland, OR 97232", "Providore Fine Foods", "Shop", "6 PM", "10 AM" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 6);
+        }
+    }
+}
--- LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
+++ LocalBusinessAPI/Models/LocalBusinessAPIContext.cs
@@ -15,7 +15,12 @@ namespace LocalBusinessAPI.Models
     {
       builder.Entity<Business>()
           .HasData(
-            new Business { BusinessId = 1, BusinessName = "United States", BusinessType = "New York City", BusinessAddress = "123 Main", OpeningHour = "11 AM", ClosingHour = "6 PM"}
+            new Business { BusinessId = 1, BusinessName = "Toro Bravo", BusinessType = "Restaurant", BusinessAddress = "120 NE Russell St, Portland, OR 97212", OpeningHour = "11 AM", ClosingHour = "6 PM"},
+            new Business { BusinessId = 2, BusinessName = "Kachka", BusinessType = "Restaurant", BusinessAddress = "960 SE 11th Ave, Portland, OR 97214", OpeningHour = "12 PM", ClosingHour = "8 PM" },
+            new Business { BusinessId = 3, BusinessName = "BAES", BusinessType = "Restaurant", BusinessAddress = "225 SW Ash St, Portland, OR 97204", OpeningHour = "11 AM", ClosingHour = "11 PM" },
+            new Business { BusinessId = 4, BusinessName = "ROAR Cat Specialty Gift Shop", BusinessType = "Shop", BusinessAddress = "3012 NE Alberta St, Portland, OR 97211", OpeningHour = "11 AM", ClosingHour = "6 PM" },
+            new Business { BusinessId = 5, BusinessName = "MadeHere PDX", BusinessType = "Shop", BusinessAddress = "40 NW 10th Ave, Portland, OR 97209", OpeningHour = "11 AM", ClosingHour = "6 PM" },
+            new Business { BusinessId = 6, BusinessName = "Providore Fine Foods", BusinessType = "Shop", BusinessAddress = "2340 NE Sandy Blvd, Portland, OR 97232", OpeningHour = "10 AM", ClosingHour = "6 PM" }
           );
     }
   }
